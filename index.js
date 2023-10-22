4906e5de4c1037e0868a4b7ae7c8748a73bb1bd7
Jonas Geiler <contact@skayo.dev>
2023-10-22 12:45:44 +0200

    chore: turned into ESM module, added PNPM support, added linter and support .js files

---
+++ index.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+import childProcess from 'node:child_process';
+import fs from 'node:fs';
+import module from 'node:module';
+import path from 'node:path';
+import process from 'node:process';
+
+// This code is forked from the `tsc-files` package and modified, so it's an
+// ESM module and works with `.js` files and PNPM.
+
+// Create a require function that works with ESM
+const require = module.createRequire(import.meta.url);
+
+/**
+ * Generate a random string of characters.
+ * @returns {string}
+ */
+const randomChars = () => Math.random().toString(36).slice(2);
+
+/**
+ * Resolve a path from a module.
+ * @param {string} moduleName
+ * @param {...string} paths
+ * @returns {string}
+ */
+const resolveFromModule = (moduleName, ...paths) => {
+	const modulePath = path.dirname(require.resolve(`${moduleName}/package.json`));
+	return path.join(modulePath, ...paths);
+};
+
+/**
+ * Resolve a path from the root of the project.
+ * @param {...string} paths
+ * @returns {string}
+ */
+const resolveFromRoot = (...paths) => path.join(process.cwd(), ...paths);
+
+// Get arguments without the node executable and this script
+const args = process.argv.slice(2);
+
+// Get the project argument and its value
+const argsProjectIndex = args.findIndex(arg =>
+	[ '-p', '--project' ].includes(arg),
+);
+const argsProjectValue = argsProjectIndex === -1
+	? undefined
+	: args[argsProjectIndex + 1];
+
+// Get the files to type-check
+const files = args.filter(file => /\.(js|mjs|cjs|jsx|ts|mts|cts|tsx)$/.test(file));
+if (files.length === 0) {
+	console.log('No files found.');
+	process.exit(0);
+}
+
+// Get the arguments to forward to tsc
+const remainingArgsToForward = args.filter(arg => !files.includes(arg));
+if (argsProjectIndex !== -1) {
+	remainingArgsToForward.splice(argsProjectIndex, 2);
+}
+
+// Load existing config
+const tsconfigPath = argsProjectValue || resolveFromRoot('tsconfig.json');
+const tsconfigContent = fs.readFileSync(tsconfigPath).toString();
+
+// Evaluate the content as JS to support comments in the config file
+const tsconfig = new Function(`return ${tsconfigContent}`)();
+
+// Create a new temporary config file with the files to type-check
+const temporaryTsconfigPath = resolveFromRoot(`tsconfig.${randomChars()}.json`);
+const temporaryTsconfig = {
+	...tsconfig,
+	compilerOptions: {
+		...tsconfig.compilerOptions,
+		skipLibCheck: true,
+	},
+	files,
+	include: [],
+};
+fs.writeFileSync(
+	temporaryTsconfigPath,
+	JSON.stringify(temporaryTsconfig),
+);
+
+// Attach cleanup handlers to remove the temporary config file on exit
+let didCleanup = false;
+for (const eventName of [ 'exit', 'SIGHUP', 'SIGINT', 'SIGTERM' ]) {
+	process.on(eventName, exitCode => {
+		if (didCleanup) {
+			return;
+		}
+
+		didCleanup = true;
+		fs.unlinkSync(temporaryTsconfigPath);
+
+		if (eventName !== 'exit') {
+			process.exit(exitCode);
+		}
+	});
+}
+
+// Resolve tsc executable
+let tsc = '';
+if (process.versions.pnp) {
+	tsc = 'tsc';
+} else {
+	tsc = resolveFromModule(
+		'typescript',
+		`../.bin/tsc${process.platform === 'win32' ? '.cmd' : ''}`,
+	);
+
+	if (!fs.existsSync(tsc)) {
+		tsc = resolveFromModule(
+			'typescript',
+			`./bin/tsc${process.platform === 'win32' ? '.cmd' : ''}`,
+		);
+	}
+
+	if (!fs.existsSync(tsc)) {
+		console.error('Failed to resolve tsc executable.');
+		process.exit(1);
+	}
+}
+
+// Type-check our files
+const result = childProcess.spawnSync(
+	tsc,
+	[ '-p', temporaryTsconfigPath, ...remainingArgsToForward ],
+	{ stdio: 'inherit' },
+);
+
+// Check if tsc failed to spawn
+if (result.status === null) {
+	console.error('Failed to spawn tsc.');
+	// DEBUG: console.debug(result);
+	process.exit(1);
+}
+
+// Exit with the same status code as tsc
+process.exit(result.status);
